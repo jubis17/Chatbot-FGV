20373d6b5ac281a960764195b0b32fa537f2973c
jubis17 <ottorodriguez29@gmail.com>
2025-10-08 19:06:05 -0300

    teste server local com sucesso

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
 import asyncio
 import logging
+from dotenv import load_dotenv
 from fastapi import FastAPI, Request, HTTPException
 from telegram import Update
 from telegram.ext import Application, ApplicationBuilder, CommandHandler, CallbackQueryHandler
+from contextlib import asynccontextmanager
 import bot as mybot
 
-api = FastAPI()
+load_dotenv()
 
-@api.get("/health")
-def health():
-    return {"status": "ok"}
+
 
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
 if not WEBHOOK_SECRET:
@@ -21,14 +21,31 @@ telegram_app: Application = ApplicationBuilder().token(mybot.TELEGRAM_TOKEN).bui
 telegram_app.add_handler(CommandHandler("start", mybot.start))
 telegram_app.add_handler(CallbackQueryHandler(mybot.tratar_clique))
 
-asyncio.get_event_loop().run_until_complete(telegram_app.initialize())
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Inicializa e inicia o bot quando o FastAPI sobe
+    await telegram_app.initialize()
+    await telegram_app.start()
+
+    try:
+        yield
+    finally:
+        # Para e encerra limpo quando servidor desliga
+        await telegram_app.stop()
+        await telegram_app.shutdown()
+
+api = FastAPI(lifespan=lifespan)
+
+@api.get("/health")
+def health():
+    return {"status": "ok"}
 
 @api.post("/webhook/{secret}")
 
 async def telegram_webhook(request: Request, secret: str):
     if secret != WEBHOOK_SECRET:
         raise HTTPException(status_code=403, detail="Forbidden")
-
+    
     try:
         data = await request.json()
         update = Update.de_json(data, telegram_app.bot)
---
+++ tunnel.py
@@ -0,0 +1,19 @@
+import os
+from pyngrok import ngrok, conf
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AUTHTOKEN = os.getenv("NGROK_AUTH_TOKEN")
+
+# 1) cole seu token aqui
+conf.get_default().auth_token = "AUTHTOKEN"
+
+
+# 2) abre túnel HTTPS para o FastAPI em 8000
+tunnel = ngrok.connect(8000, bind_tls=True)
+print("PUBLIC URL:", tunnel.public_url)
+
+input("\nPressione Enter para encerrar o túnel...")
+ngrok.disconnect(tunnel.public_url)
+ngrok.kill()
